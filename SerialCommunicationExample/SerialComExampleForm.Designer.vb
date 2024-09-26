9cd1748faabd0b5423c5c41f52a923a774167c29
Zac <zac.minidude140@Gmail.com>
2024-09-26 10:27:40 -0600

    added qw@ board digital out interface

--- SerialCommunicationExample/SerialComExampleForm.Designer.vb
+++ SerialCommunicationExample/SerialComExampleForm.Designer.vb
@@ -22,10 +22,35 @@ Partial Class SerialComExampleForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.DiscoLightsButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'SerialPort1
+        '
+        '
+        'DiscoLightsButton
+        '
+        Me.DiscoLightsButton.Location = New System.Drawing.Point(304, 195)
+        Me.DiscoLightsButton.Name = "DiscoLightsButton"
+        Me.DiscoLightsButton.Size = New System.Drawing.Size(160, 76)
+        Me.DiscoLightsButton.TabIndex = 0
+        Me.DiscoLightsButton.Text = "Start A " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "DISCO!!"
+        Me.DiscoLightsButton.UseVisualStyleBackColor = True
+        '
+        'SerialComExampleForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DiscoLightsButton)
+        Me.Name = "SerialComExampleForm"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents DiscoLightsButton As Button
 End Class
--- SerialCommunicationExample/SerialComExampleForm.vb
+++ SerialCommunicationExample/SerialComExampleForm.vb
@@ -1,3 +1,46 @@
-﻿Public Class SerialComExampleForm
+﻿Imports System.IO.Ports
+Imports System.Threading
+Public Class SerialComExampleForm
+    Private Sub SerialComExampleForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Set up port name and baud rate
+        SerialPort1.PortName = "COM5"
+        SerialPort1.BaudRate = 9600
+        'open serial port for data transfer
+        SerialPort1.Open()
+    End Sub
 
+    Private Sub DiscoLightsButton_Click(sender As Object, e As EventArgs) Handles DiscoLightsButton.Click
+        DiscoLightsButton.Enabled = False
+        For i = 0 To 10
+            WriteDigitalOut(&HAA)
+            Thread.Sleep(500)
+            WriteDigitalOut(&H55)
+            Thread.Sleep(500)
+        Next
+        WriteDigitalOut(&H0)
+        DiscoLightsButton.Enabled = True
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        'Read from serial port (event triggers after so many bytes have been received)
+        'create an array of bytes with the length of given bytes
+        Dim data(SerialPort1.BytesToRead) As Byte
+        'populates data with incoming port data starting at 0 bit and reading the full bytes
+        SerialPort1.Read(data, 0, SerialPort1.BytesToRead)
+        'loops through the data array and writes each item to console (converts to hex)
+        For i = 0 To UBound(data)
+            Console.Write($"{Hex(data(i))}: ")
+            Console.WriteLine(Chr(data(i)))
+
+        Next
+    End Sub
+    Sub WriteDigitalOut(output As Byte)
+        'create a byte array
+        Dim data(1) As Byte
+        'populate the byte array
+        data(0) = &H20
+        data(1) = output
+        'write the data byte array starting at bit 0 and sending one byte
+        SerialPort1.Write(data, 0, 2)
+    End Sub
 End Class
